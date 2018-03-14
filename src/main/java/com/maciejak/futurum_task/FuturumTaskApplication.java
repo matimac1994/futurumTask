e753d81eb4d7313bdd0dfa1250ce30225a125e00
Mati <maciejak.dev@gmail.com>
2018-03-14 10:15:47 +0100

    Initial commit

---
+++ src/main/java/com/maciejak/futurum_task/FuturumTaskApplication.java
@@ -0,0 +1,12 @@
+package com.maciejak.futurum_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturumTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturumTaskApplication.class, args);
+	}
+}
